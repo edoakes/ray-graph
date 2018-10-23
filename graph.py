32c7fd4ea7e87f6ebec769caad089103f2388da2
Ed Oakes <ed.nmi.oakes@gmail.com>
2018-10-23 16:13:59 -0700

    Parse actor and task definitions

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 class RayGraphNode(object):
     def __init__(self, name):
         self.name = name
+        self.deps = []
 
 class Object(RayGraphNode):
     pass
--- parser.py
+++ parser.py
@@ -9,8 +9,9 @@ class GraphParser(ast.NodeVisitor):
     """
 
     def __init__(self):
-        self.graph = []
-        self.sym_table = {}
+        self.graph = {}
+        self.task_defs = set()
+        self.actor_defs = set()
 
     def generic_visit(self, node):
         pass
@@ -19,16 +20,29 @@ class GraphParser(ast.NodeVisitor):
         pass
 
     def visit_If(self, node):
-        pass
+        for n in node.body+node.orelse:
+            self.visit(n)
 
     def visit_For(self, node):
-        pass
-
-    def visit_Return(self, node):
-        pass
+        for n in node.body:
+            self.visit(n)
 
     def visit_ClassDef(self, node):
-        pass
+        for d in node.decorator_list:
+            if d.value.id == 'ray' and d.attr == 'remote':
+                self.actor_defs.add(node.name)
+
+        for n in node.body:
+            self.visit(n)
 
     def visit_FunctionDef(self, node):
-        pass
+        for d in node.decorator_list:
+            if d.value.id == 'ray' and d.attr == 'remote':
+                self.task_defs.add(node.name)
+
+        for n in node.body:
+            self.visit(n)
+
+    def visit_Module(self, node):
+        for n in node.body:
+            self.visit(n)
