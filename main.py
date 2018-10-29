edeb0b1376f7877ab0773c69d0c99852aef2c926
Ed Oakes <ed.nmi.oakes@gmail.com>
2018-10-29 16:01:38 -0700

    Separate parsing into stages

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import ast
 import argparse
 import sys
 
-from parser import GraphParser
+from parser import RayDefParser, GlobalGraphParser
 
 parser = argparse.ArgumentParser(description='Parse dependency graph from a Ray application file.')
 parser.add_argument('input', help='Ray application file to parse.')
@@ -11,9 +11,11 @@ def main(args):
     with open(args.input) as fh:
         tree = ast.parse(fh.read())
 
-    gp = GraphParser()
-    gp.visit(tree)
-    print(gp.graph)
+    tasks, actors = RayDefParser().visit(tree)
+    print('tasks: %s' % tasks)
+    print('actors: %s' % actors)
+
+    graph = GlobalGraphParser(tasks, actors).visit(tree)
 
 if __name__ == '__main__':
     main(parser.parse_args())
--- parser.py
+++ parser.py
@@ -1,48 +1,99 @@
 import ast
-import symtable
 
 from graph import *
 
-class GraphParser(ast.NodeVisitor):
+class RayDefParser(ast.NodeVisitor):
     """
-    Translate a Ray application file into a dependency graph.
+    Parses and returns globally-defined Ray tasks and actors.
+
+    Returns a separate dictionary of names to AST nodes for tasks and actors.
     """
 
     def __init__(self):
-        self.graph = {}
-        self.task_defs = set()
-        self.actor_defs = set()
+        self.tasks = {}
+        self.actors = {}
 
-    def generic_visit(self, node):
-        pass
-
-    def visit_Assign(self, node):
-        pass
-
-    def visit_If(self, node):
-        for n in node.body+node.orelse:
-            self.visit(n)
-
-    def visit_For(self, node):
-        for n in node.body:
-            self.visit(n)
-
-    def visit_ClassDef(self, node):
-        for d in node.decorator_list:
-            if d.value.id == 'ray' and d.attr == 'remote':
-                self.actor_defs.add(node.name)
-
-        for n in node.body:
-            self.visit(n)
+    def _is_ray_def(self, decorator):
+        return decorator.value.id == 'ray' and decorator.attr == 'remote'
 
     def visit_FunctionDef(self, node):
         for d in node.decorator_list:
-            if d.value.id == 'ray' and d.attr == 'remote':
-                self.task_defs.add(node.name)
+            if self._is_ray_def(d):
+                self.tasks[node.name] = node
 
-        for n in node.body:
-            self.visit(n)
+    def visit_ClassDef(self, node):
+        for d in node.decorator_list:
+            if self._is_ray_def(d):
+                self.actors[node.name] = node
 
     def visit_Module(self, node):
         for n in node.body:
             self.visit(n)
+        return self.tasks, self.actors
+
+class GraphParser(ast.NodeVisitor):
+    """
+    Base class for parsing a dependency graph from a Python AST.
+    """
+
+    def __init__(self, tasks, actors):
+        self.graph = {}
+        self.tasks = tasks
+        self.actors = actors
+
+class FunctionSubgraphParser(GraphParser):
+    """
+    Parses a dependency subgraph from a Python function.
+
+    Returns a single graph.
+    """
+
+    # Entrypoint.
+    def visit_FunctionDef(self, node):
+        pass
+
+class ActorSubgraphParser(GraphParser):
+    """
+    Parses a dependency subgraph for each method of a Ray actor.
+
+    Returns a dictionary of methods to graphs.
+    """
+
+    # Entrypoint.
+    def visit_ClassDef(self, node):
+        pass
+
+class GlobalGraphParser(GraphParser):
+    """
+    Translate a Ray application file into a dependency graph.
+    """
+
+    def __init__(self, tasks, actors, function='main'):
+        super().__init__(tasks, actors)
+        self.function = function
+        self.task_subgraphs = {}
+        self.actor_subgraphs = {}
+        self.actor_parser = ActorSubgraphParser(self.tasks, self.actors)
+        self.function_parser = FunctionSubgraphParser(self.tasks, self.actors)
+
+    # In the first pass, call nodes in the graph are just references. Here, we
+    # expand them to include their parsed dependency information.
+    def _expand_graph(self):
+        pass
+
+    def visit_FunctionDef(self, node):
+        if node.name != self.function:
+            return
+        self.graph = FunctionSubgraphParser(self.tasks, self.actors).visit(node)
+
+    # Entrypoint.
+    def visit_Module(self, node):
+        for name,node in self.tasks.items():
+            self.task_graphs[name] = self.function_parser.visit(node)
+        for name,node in self.actors.items():
+            self.actor_subgraphs[name] = self.actor_parser.visit(node)
+        for n in node.body:
+            self.visit(n)
+
+        self._expand_graph()
+        return self.graph
